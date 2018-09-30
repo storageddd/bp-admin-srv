4f670bd2e8dc0a74030cb1f77c22c98e7573012b
storaged <storaged@mail.ru>
2018-09-30 23:40:27 +0300

    init

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const port = process.env.PORT || 8080;
+
+app.use('/static', express.static(path.join(__dirname, 'static')));
+app.use('/static', express.static(path.join(__dirname, 'stubs')));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/index.html')));
+app.get('/order/*', (req, res) => res.sendFile(path.join(__dirname+'/index.html')));
+app.get('/orders', (req, res) => res.sendFile(path.join(__dirname+'/index.html')));
+
+app.get('/api/config', (req, res) => {
+  if (!isAuthorizationTokenValid(req.headers.authorization)) return;
+
+  let data = require('./stubs/config.json');
+  return res.send(data);
+});
+
+app.get('/api/orders', (req, res) => {
+  if (!isAuthorizationTokenValid(req.headers.authorization)) return;
+
+  let data = require('./stubs/orders.json');
+  return res.send(data);
+});
+
+app.get('/api/order/*', (req, res) => {
+  if (!isAuthorizationTokenValid(req.headers.authorization)) return;
+
+  let data = require('./stubs/order.json');
+  return res.send(data);
+});
+
+app.get('/api/search_orders', (req, res) => {
+  if (!isAuthorizationTokenValid(req.headers.authorization)) return;
+
+  let data = require('./stubs/search.json');
+  return res.send(data);
+});
+
+function isAuthorizationTokenValid(token) {
+  return true;
+}
+
+app.post('/api/cancel_order', (req, res) => { console.log(req.body); return res.sendStatus(200) });
+app.post('/api/change_order_status', (req, res) => { console.log(req.body); return res.sendStatus(200) });
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
